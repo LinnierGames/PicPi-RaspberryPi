bb71f0b55dc18f63f879a70ebe257826df0ff0a4
LinnierGames <esericksanc@gmail.com>
2020-12-11 23:11:46 -0800

    Use multer to store images

--- nodejs-server/routes/index.js
+++ nodejs-server/routes/index.js
@@ -1,6 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var MQTT = require('./MQTT');
+var multer  = require('multer');
 var DI = require('./../src/DI');
+
+const upload = multer({ dest: "/home/pi/Pictures/.temp" })
+
+router.post('/photos/upload-2', upload.single("file"), function(req, res, next) {
+  const isPayloadEmpty = Object.keys(req.file).length === 0;
+  if (isPayloadEmpty) {
+    res.status(400).json({ message: "format is incorrect" });
+    return
+  }
+
+  if (req.file.path === undefined) {
+    res.status(400).json({ message: "missing image" });
+    return
+  }
+
+  if (req.file.filename === undefined) {
+    res.status(400).json({ message: "missing filename" });
+    return
+  }
+
+  const tempPath = req.file.path;
+  const targetPath = path.join("/home/pi/Pictures", req.file.originalname);;
+
+  fs.rename(tempPath, targetPath, err => {
+    if (err) return res.status(500).json({ message: err });
+
+    const mqtt = new MQTT('localhost');
+    mqtt.publish("OK", 'file-system/photos/did-update')
+
+    res
+      .status(200)
+      .json({ message: "Success!", filename: req.file.originalname });
+  });
+});
 
 router.post('/photos/upload', function(req, res, next) {
   const isPayloadEmpty = Object.keys(req.body).length === 0;
